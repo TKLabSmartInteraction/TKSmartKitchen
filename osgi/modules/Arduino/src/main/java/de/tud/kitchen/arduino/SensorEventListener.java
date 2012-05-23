c7b7fe523f481c9a5e495d9ce62323aad9202a0b
Christian <christianklos@gmx.net>
2012-05-23 15:29:26 +0200

    create arduino OSGI bundle

---
+++ osgi/modules/Arduino/src/main/java/de/tud/kitchen/arduino/SensorEventListener.java
@@ -0,0 +1,11 @@
+package de.tud.kitchen.arduino;
+
+public interface SensorEventListener {
+	
+	/**
+	 * this method is called when sensor data arrives
+	 * @param arg0 the sensor data that arrives
+	 */
+	public void SensorEvent(SensorEvent arg0);
+
+}
