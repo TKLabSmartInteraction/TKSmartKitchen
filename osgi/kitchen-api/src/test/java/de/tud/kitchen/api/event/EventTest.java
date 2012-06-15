78dbe6517765191030153f890ed22bd6bb30ce2d
Niklas Lochschmidt <nlochschmidt@gmail.com>
2012-06-15 09:12:14 +0200

    caching methods for faster invocation add stresstests to evaluate caching improvements

--- osgi/kitchen-api/src/test/java/de/tud/kitchen/api/event/EventTest.java
+++ osgi/kitchen-api/src/test/java/de/tud/kitchen/api/event/EventTest.java
@@ -8,7 +8,7 @@ public class EventTest {
 	@Test
 	public void dispatchTest() {
 		//SETUP
-		EventConsumer eventConsumer = createMockBuilder(EventConsumer.class).addMockedMethod("handleObject").createMock();
+		EventConsumer eventConsumer = createMockBuilder(EventConsumer.class).addMockedMethod("handleObject").withConstructor().createMock();
 		Event event = new Event("testSensor") {};
 		eventConsumer.handleObject(event);
 		replay(eventConsumer);
