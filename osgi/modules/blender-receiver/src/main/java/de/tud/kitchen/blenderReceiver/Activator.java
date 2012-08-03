027ba35930b805f0e6217ca00bd8a38046d6bfa4
Christian <christianklos@gmx.net>
2012-08-03 02:49:29 +0200

    add Header and comments and better Button Names to the blender-receiver

--- osgi/modules/blender-receiver/src/main/java/de/tud/kitchen/blenderReceiver/Activator.java
+++ osgi/modules/blender-receiver/src/main/java/de/tud/kitchen/blenderReceiver/Activator.java
@@ -1,12 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Contributor(s):
+ *    Marcus Staender <staender@tk.informatik.tu-darmstadt.de>
+ *    Aristotelis Hadjakos <telis@tk.informatik.tu-darmstadt.de>
+ *    Niklas Lochschmidt <nlochschmidt@gmail.com>
+ *    Christian Klos <christian.klos@stud.tu-darmstadt.de>
+ *    Bastian Renner <bastian.renner@stud.tu-darmstadt.de>
+ *
+ */
+
 package de.tud.kitchen.blenderReceiver;
 
 import org.mundo.rt.Mundo;
-import org.osgi.framework.BundleContext;
 
 import de.tud.kitchen.api.Kitchen;
 import de.tud.kitchen.api.module.KitchenModuleActivator;
 
-
+/**
+ * Just the Activator for the BlenderReceiver, it starts the modules that
+ * are necessary to recieve and process BlenderEvents
+ * @author Christian Klos
+ *
+ */
 public class Activator extends KitchenModuleActivator {
 	
 	@Override
--- osgi/modules/blender-receiver/src/main/java/de/tud/kitchen/blenderReceiver/BlenderEvent.java
+++ osgi/modules/blender-receiver/src/main/java/de/tud/kitchen/blenderReceiver/BlenderEvent.java
@@ -1,13 +1,57 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Contributor(s):
+ *    Marcus Staender <staender@tk.informatik.tu-darmstadt.de>
+ *    Aristotelis Hadjakos <telis@tk.informatik.tu-darmstadt.de>
+ *    Niklas Lochschmidt <nlochschmidt@gmail.com>
+ *    Christian Klos <christian.klos@stud.tu-darmstadt.de>
+ *    Bastian Renner <bastian.renner@stud.tu-darmstadt.de>
+ *
+ */
+
 package de.tud.kitchen.blenderReceiver;
 
 import de.tud.kitchen.api.event.Event;
 
+/**
+ * represents a button push or release
+ * @author Christian Klos
+ *
+ */
 public class BlenderEvent extends Event {
 	
 	boolean pushed;
+	String prettyButtonName;
 	
 	public BlenderEvent(String sender, boolean pushed) {		
 		super(sender);
+		int buttonNumber=new Integer(sender);
+		switch (buttonNumber) {
+		case 01:
+			prettyButtonName = "Smoothy";
+			break;
+		case 02:
+			prettyButtonName = "Ice";
+			break;
+		case 04:
+			prettyButtonName = "Pulse";
+			break;
+		case 8:
+			prettyButtonName = "Dial-on";
+			break;
+		case 10:
+			prettyButtonName = "Dial-Min...Max";
+			break;		
+		case 20:
+			prettyButtonName = "Motor";
+			break;
+		default:
+			prettyButtonName = sender;
+			break;
+		}
+		
 		this.pushed = pushed;
 	}
 	
@@ -18,7 +62,7 @@ public class BlenderEvent extends Event {
 
 	@Override
 	public String toString() {
-		return "Button "+this.sender+(pushed ? " pushed" : " released");
+		return "Button "+this.prettyButtonName+(pushed ? " pushed" : " released");
 	}
 
 	@Override
